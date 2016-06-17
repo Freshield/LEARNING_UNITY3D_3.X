8efacb8dab0d4bacffed3c539e4a7e4523afcb11
Freshield <zxdsw199182@gmail.com>
2016-06-16 21:16:28 -0400

    test18

--- chp3/test1/Assets/18.js
+++ chp3/test1/Assets/18.js
@@ -4,6 +4,8 @@ function Start () {
 
 }
 
-function Update () {
+function OnGUI () {
+
+	GUI.Label(Rect(0, 10, 100, 30),"javascript中文测试");
 
 }
---
+++ chp3/test1/Assets/GUI18.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUI18 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI () {
+
+		GUI.Label(new Rect(1, 50, 100, 30),"C#中文测试");
+
+	}
+}
