ee5a8536366b26112aebc69915bcc33e14add984
Freshield <zxdsw199182@gmail.com>
2016-06-17 21:08:06 -0400

    test17

---
+++ chp3/test1/Assets/17.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var mySkin : GUISkin;
+
+function Start () {
+
+}
+
+function OnGUI () {
+
+	GUI.skin = mySkin;
+
+	GUILayout.Box("华文宋体");
+
+	GUILayout.Button("华文楷体");
+
+	GUILayout.Label("默认字体");
+}
