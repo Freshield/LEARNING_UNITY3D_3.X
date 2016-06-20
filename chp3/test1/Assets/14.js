1d11d6d2754207a6c6941a45e8049e2b9faea341
Freshield <zxdsw199182@gmail.com>
2016-06-20 16:52:55 -0400

    test14

---
+++ chp3/test1/Assets/14.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function OnGUI () {
+
+	GUILayout.BeginArea(Rect(0, 0, 200, 60));
+	GUILayout.BeginHorizontal();
+	GUILayout.BeginVertical();
+	GUILayout.Box("test1");
+
+	GUILayout.Space(10);
+	GUILayout.Box("text2");
+	GUILayout.EndVertical();
+
+	GUILayout.Space(20);
+
+	GUILayout.BeginVertical();
+	GUILayout.Box("test3");
+
+	GUILayout.Space(10);
+	GUILayout.Box("test4");
+
+	GUILayout.EndVertical();
+
+	GUILayout.EndHorizontal();
+
+	GUILayout.EndArea();
+}
