6170d7896cb96af6f3bd043e9424421c6d50db96
Freshield <zxdsw199182@gmail.com>
2016-06-21 16:25:43 -0400

    test13

---
+++ chp3/test1/Assets/13.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+var texture : Texture2D;
+
+function Start () {
+
+}
+
+function OnGUI () {
+
+	GUILayout.BeginHorizontal();
+	GUILayout.Box("begin horizontal");
+	GUILayout.Button("button");
+	GUILayout.Label("label");
+	GUILayout.TextField("textfield");
+	GUILayout.Box(texture);
+	GUILayout.EndHorizontal();
+
+	GUILayout.BeginVertical();
+	GUILayout.Box("begin vertical");
+	GUILayout.Button("button");
+	GUILayout.Label("label");
+	GUILayout.TextField("textfield");
+	GUILayout.Box(texture);
+	GUILayout.EndVertical();
+
+
+}
