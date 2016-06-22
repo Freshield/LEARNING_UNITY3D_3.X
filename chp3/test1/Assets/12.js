7899781e458acc731957cf66138155f4df063442
Freshield <zxdsw199182@gmail.com>
2016-06-22 16:07:22 -0400

    test12

---
+++ chp3/test1/Assets/12.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function OnGUI () {
+
+	GUILayout.Button("width 300, height 30", GUILayout.Width(300),GUILayout.Height(30));
+	GUILayout.Button("minwidth 100, minheight 20", GUILayout.MinWidth(100),GUILayout.MinHeight(20));
+	GUILayout.Button("maxwidth 400, maxheight 40", GUILayout.MaxWidth(400),GUILayout.MaxHeight(40));
+	GUILayout.Button("expandwidth false", GUILayout.ExpandWidth(false));
+	GUILayout.Button("expandwidth true", GUILayout.ExpandWidth(true));
+
+}
