e825939b7aa533c58bb1b5a999c20dde83212c9e
Freshield <zxdsw199182@gmail.com>
2016-06-24 23:09:53 -0400

    test scrollview

---
+++ chp3/test1/Assets/06.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+var scrollPosition : Vector2;
+
+function Start () {
+
+	scrollPosition[0] = 50;
+	scrollPosition[1] = 50;
+
+}
+
+
+
+function OnGUI () {
+
+	scrollPosition = GUI.BeginScrollView(Rect(0, 0, 300, 200), scrollPosition, Rect(0, 0, Screen.width, 300), true, true);
+
+	GUI.Label(Rect(100, 40, Screen.width, 30), "test scroll view, test scroll view, test scroll view, test scroll view");
+
+	GUI.EndScrollView();
+
+}
