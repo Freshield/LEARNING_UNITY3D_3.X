5c4834d829cde5fd486555bf22e5c1cd3024095c
Freshield <zxdsw199182@gmail.com>
2016-07-08 18:35:28 -0400

    write a js

---
+++ chp2/test1/CodeList_2_1/Assets/script_02_01.js
@@ -0,0 +1,44 @@
+﻿#pragma strict
+
+var TranslateSpeed = 20;
+
+var RotateSpeed = 1000;
+
+function OnGUI()
+{
+    Gui.backgroundColor = Color.red;
+
+    if (GUI.Button(Rect(10, 10, 70, 30), "turn left")) 
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * (-RotateSpeed));
+    }
+
+    if (GUI.Button(Rect(90, 10, 70, 30), "move forward")) 
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * TranslateSpeed);
+    }
+
+    if (GUI.Button(Rect(170, 10, 70, 30), "turn right")) 
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * RotateSpeed);
+    }
+
+    if (GUI.Button(Rect(90, 50, 70, 30), "move backward")) 
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * (-TranslateSpeed));
+    }
+    
+    if (GUI.Button(Rect(10, 50, 70, 30), "move left")) 
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * (-TranslateSpeed));
+    }
+    
+    if (GUI.Button(Rect(170, 50, 70, 30), "move right")) 
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * TranslateSpeed);
+    }
+
+    GUI.Label(Rect(250,10,20,30),"model position" + transform.position);
+
+    GUI.Label(Rect(250,50,200,30),"model rotation" + transform.rotation);
+}
