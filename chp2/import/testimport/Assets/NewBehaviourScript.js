a526db859fa1ce12c17e43cd13f7e1b20f9be124
Freshield <zxdsw199182@gmail.com>
2016-07-09 18:06:56 -0400

    test import

---
+++ chp2/import/testimport/Assets/NewBehaviourScript.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+    transform.Translate(Vector3.forward * Time.deltaTime * 2);
+    transform.Rotate(Vector3.up * Time.deltaTime * 20);
+}
