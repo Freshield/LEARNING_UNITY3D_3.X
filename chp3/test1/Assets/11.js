5effed7a34f3a0a20a48d3388c7e1b0767d0a0e2
Freshield <zxdsw199182@gmail.com>
2016-07-15 16:00:01 -0400

    test11

---
+++ chp3/test1/Assets/11.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+var addStr : String = "add test string";
+
+function Start () {
+
+}
+
+function OnGUI () {
+
+	if(GUI.Button(Rect(50, 50, 100, 30), addStr))
+	{
+		addStr  += addStr;
+	}
+
+	if(GUILayout.Button(addStr))
+	{
+		addStr += addStr;
+	}
+
+}
