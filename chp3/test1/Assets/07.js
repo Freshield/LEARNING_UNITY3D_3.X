87af6541856a564bba8cb5722ee7934b4c865b3f
Freshield <zxdsw199182@gmail.com>
2016-07-16 17:30:16 -0400

    done window

---
+++ chp3/test1/Assets/07.js
@@ -0,0 +1,39 @@
+﻿#pragma strict
+
+var viewTexture0 : Texture2D;
+
+var viewTexture1 : Texture2D;
+
+function Start () {
+
+}
+
+function OnGUI () {
+
+	GUI.BeginGroup(new Rect(10, 50,200, 400));
+
+	GUI.DrawTexture(Rect(10, 20, viewTexture0.width, viewTexture0.height), viewTexture0);
+
+	GUI.Label(Rect(10, 150, 100, 40), "group1");
+
+	GUI.Button(Rect(10, 190, 100, 40), "button");
+
+	GUI.EndGroup();
+
+
+
+	GUI.BeginGroup(new Rect(300, 0,500, 400));
+
+	GUI.DrawTexture(Rect(10, 20, viewTexture1.width, viewTexture1.height), viewTexture1);
+
+	GUI.Label(Rect(10, 150, 100, 40), "group2");
+
+	GUI.Button(Rect(10, 190, 100, 40), "button");
+
+	GUI.EndGroup();
+
+
+
+
+
+}
---
+++ chp3/test1/Assets/08.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+
+private var window0 : Rect = Rect (20, 20, 200, 200);
+private var window1 : Rect = Rect (250, 20, 200, 200);
+
+function Start () {
+
+}
+
+function OnGUI () {
+
+	GUI.Window( 0, window0, oneWindow, "first window");
+	GUI.Window( 1, window1, twoWindow, "second window");
+
+}
+
+function oneWindow(windowID : int)
+{
+	GUI.Box(Rect(10, 50, 150, 50), " this window's ID is: " + windowID);
+	if(GUI.Button(Rect(10, 120, 150, 50), "normal button"))
+	{
+		Debug.Log("window ID " + windowID);
+	}
+}
+
+function twoWindow(windowID : int)
+{
+	GUI.Box(Rect(10, 50, 150, 50), " this window's ID is: " + windowID);
+	if(GUI.Button(Rect(10, 120, 150, 50), "normal button"))
+	{
+		Debug.Log("window ID " + windowID);
+	}
+}
