613261984e9f13f69b4cc67ee52d03023c387bee
Freshield <zxdsw199182@gmail.com>
2016-07-17 00:54:40 -0400

    done today

---
+++ chp3/test1/Assets/09.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+
+var myskin : GUISkin;
+
+private var choose : boolean = false;
+
+var windowRect : Rect = Rect(20, 20, 220, 250);
+
+var edit : String = "please input words";
+
+function Start () {
+
+}
+
+function OnGUI () {
+
+	GUI.skin = myskin;
+
+	GUI.Button(Rect(100,100,100,100), "button");
+
+	choose = GUI.Toggle(Rect(10, 50, 100, 30), choose, "toggle");
+
+	edit = GUI.TextField(Rect(200, 10, 200, 20), edit, 25);
+
+	windowRect = GUI.Window(0, windowRect, setWindow, "this is a window");
+
+}
+
+function setWindow(windowID : int)
+{
+	GUI.DragWindow();
+	GUI.Button(Rect(10,20,100,30),"button");
+}
