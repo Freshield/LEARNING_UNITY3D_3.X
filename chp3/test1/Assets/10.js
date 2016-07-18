eab7a0740c20e460e459a7a4cf36ad82d0e28ce6
Freshield <zxdsw199182@gmail.com>
2016-07-18 15:46:52 -0400

    done test10

---
+++ chp3/test1/Assets/10.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var mySkin : GUISkin;
+
+function Start () {
+
+}
+
+function OnGUI () {
+
+	GUI.skin = mySkin;
+
+	GUI.Button(Rect(330, 100, 130, 110),"", "Custom1");
+	GUI.Button(Rect(0, 100, 130, 110),"","Custom0");
+
+}
