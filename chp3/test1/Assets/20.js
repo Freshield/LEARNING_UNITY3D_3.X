310ddc00ea2ad257b1cb7e168d057b4fc7a3549b
Freshield <zxdsw199182@gmail.com>
2016-07-18 22:04:11 -0400

    done today

---
+++ chp3/test1/Assets/20.js
@@ -0,0 +1,41 @@
+﻿#pragma strict
+
+private var anim : Object[];
+
+private var nowFram : int;
+
+private var mFrameCount : int;
+
+private var fps : float = 5;
+
+private var time : float = 0;
+
+function Start () {
+
+	anim = Resources.LoadAll("animation");
+
+	mFrameCount = anim.Length;
+
+}
+
+function OnGUI () {
+
+	DrawAnimation(anim,Rect(100, 100, 32, 48));
+
+}
+
+function DrawAnimation(tex:Object[], rect : Rect)
+{
+	GUILayout.Label("anima playing now: the " + nowFram + " fps");
+	GUI.DrawTexture(rect, tex[nowFram], ScaleMode.StretchToFill, true, 0);
+	time += Time.deltaTime;
+	if(time >= 1.0 / fps)
+	{
+		nowFram++;
+		time = 0;
+		if(nowFram >= mFrameCount)
+		{
+			nowFram = 0;
+		}
+	}
+}
