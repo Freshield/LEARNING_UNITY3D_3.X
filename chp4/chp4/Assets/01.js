1ccd150e30647b852e20bfd68fae6110efa04ced
Freshield <zxdsw199182@gmail.com>
2016-07-20 17:20:30 -0400

    done test1

---
+++ chp4/chp4/Assets/01.js
@@ -0,0 +1,37 @@
+﻿#pragma strict
+
+function Start () {
+
+
+}
+
+function OnGUI () {
+
+	if(GUILayout.Button("create cube",GUILayout.Height(50)))
+	{
+		var objCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+		objCube.AddComponent(Rigidbody);
+
+		objCube.name = "Cube";
+
+		objCube.GetComponent.<Renderer>().material.color = Color.blue;
+
+		objCube.transform.position = new Vector3(0.0f, 10.0f, 0.0f);
+	}
+
+	if(GUILayout.Button("create sphere",GUILayout.Height(50)))
+	{
+		var objSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+		objSphere.AddComponent(Rigidbody);
+
+		objSphere.name = "Sphere";
+
+		objSphere.GetComponent.<Renderer>().material.color = Color.red;
+
+		objSphere.transform.position = new Vector3(0.0f, 10.0f, 0.0f);
+	}
+
+
+}
