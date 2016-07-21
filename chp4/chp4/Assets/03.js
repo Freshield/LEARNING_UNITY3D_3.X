641bc3efa52c82106c633b39839a34cd03000f38
Freshield <zxdsw199182@gmail.com>
2016-07-21 15:16:06 -0400

    test 4

---
+++ chp4/chp4/Assets/03.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+private var obj : GameObject;
+
+function Start () {
+
+	obj = GameObject.FindWithTag("MyTag");
+
+}
+
+function Update () {
+
+	obj.transform.Rotate(0.0f, Time.deltaTime * 200, 0.0f);
+
+}
---
+++ chp4/chp4/Assets/04.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+function Start () {
+
+	var objs = GameObject.FindGameObjectsWithTag("MyTag");
+
+	objs[5].tag = "TestTag";
+
+	for( var obj in objs)
+	{
+		Debug.Log("use " + obj.tag + " tag as name " + obj.name);
+
+		if(obj.tag == "TestTag")
+		{
+			Debug.Log("this tag is TestTag");
+		}
+
+		if(obj.CompareTag("TestTag"))
+		{
+			Debug.Log("obj has extend tag as TestTag");
+		}
+	}
+
+}
+
+function Update () {
+
+}
