389b8fd821021bfad74ebc57aa9a9f5a448676ec
Freshield <zxdsw199182@gmail.com>
2016-07-21 20:31:25 -0400

    test06

---
+++ chp4/chp4/Assets/06.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+var obj : GameObject;
+
+var times : int = 1;
+
+function Start () {
+
+	obj = GameObject.Find("Sphere");
+
+}
+
+function OnGUI () {
+
+	if(GUILayout.Button("clone an instance", GUILayout.Height(50)))
+	{
+		var clone : GameObject = Instantiate(obj, obj.transform.position * 2 * times, obj.transform.rotation);
+
+		times ++;
+
+		Destroy(clone, 5);
+	}
+
+}
