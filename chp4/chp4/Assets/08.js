6fc78ec50a28233c9c41b03976aa0b3eb12deb90
Freshield <zxdsw199182@gmail.com>
2016-07-21 20:47:45 -0400

    test08

---
+++ chp4/chp4/Assets/08.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+private var Value_X : float = 0.0f;
+
+private var Value_Y : float = 0.0f;
+
+private var Value_Z : float = 0.0f;
+
+private var obj : GameObject;
+
+function Start () {
+
+	obj = GameObject.Find("Cube");
+
+}
+
+function OnGUI () {
+
+	GUILayout.Box("move cube x");
+	Value_X = GUILayout.HorizontalSlider(Value_X, -10.0f, 10.0f, GUILayout.Width(200));
+
+	GUILayout.Box("move cube y");
+	Value_Y = GUILayout.HorizontalSlider(Value_Y, -10.0f, 10.0f, GUILayout.Width(200));
+
+	GUILayout.Box("move cube z");
+	Value_Z = GUILayout.HorizontalSlider(Value_Z, -10.0f, 10.0f, GUILayout.Width(200));
+
+	obj.transform.position = Vector3(Value_X, Value_Y, Value_Z);
+	GUILayout.Label("the cube position now is " + obj.transform.position);
+
+}
