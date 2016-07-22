5739d88012b53313d3dd16eaec92ce5a1c09aea2
Freshield <zxdsw199182@gmail.com>
2016-07-21 20:40:49 -0400

    test07

---
+++ chp4/chp4/Assets/07.js
@@ -0,0 +1,46 @@
+﻿#pragma strict
+
+var obj : GameObject;
+
+function Start () {
+
+	obj = GameObject.Find("Cube");
+
+}
+
+function OnGUI () {
+
+	if(GUILayout.Button("add script",GUILayout.Height(50)))
+	{
+		if(obj)
+		{
+			obj.AddComponent.<cube_script>();
+		}
+	}
+
+	if(GUILayout.Button("delete script",GUILayout.Height(50)))
+	{
+		if(obj)
+		{
+			Destroy(obj.GetComponent("cube_script"));
+		}
+	}
+
+	if(GUILayout.Button("delete cube",GUILayout.Height(50)))
+	{
+		if(obj)
+		{
+			Destroy(obj);
+		}
+	}
+
+	if(GUILayout.Button("delete cube in 5 seconds",GUILayout.Height(50)))
+	{
+		if(obj)
+		{
+			Destroy(obj, 5);
+		}
+	}
+
+
+}
---
+++ chp4/chp4/Assets/cube_script.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function Start () {
+
+	Debug.Log("add success");
+
+}
+
+function OnDestroy () {
+
+	Debug.Log("delete success");
+
+}
