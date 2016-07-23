78156ed07a149030b7f4e9e5bfac166218d1ee3d
Freshield <zxdsw199182@gmail.com>
2016-07-23 16:08:18 -0400

    test10

---
+++ chp4/chp4/Assets/10.js
@@ -0,0 +1,45 @@
+﻿#pragma strict
+
+var obj : GameObject;
+
+function Start () {
+
+	obj = GameObject.Find("Cube");
+
+}
+
+function OnGUI () {
+
+	if(GUILayout.Button("move forward", GUILayout.Height(50)))
+	{
+		obj.transform.Translate(Vector3.forward * Time.deltaTime);
+	}
+
+	if(GUILayout.Button("move backward", GUILayout.Height(50)))
+	{
+		obj.transform.Translate(-Vector3.forward * Time.deltaTime);
+	}
+
+	if(GUILayout.Button("move left", GUILayout.Height(50)))
+	{
+		obj.transform.Translate(Vector3.left * Time.deltaTime);
+	}
+
+	if(GUILayout.Button("move right", GUILayout.Height(50)))
+	{
+		obj.transform.Translate(Vector3.right * Time.deltaTime);
+	}
+
+	if(GUILayout.Button("move up", GUILayout.Height(50)))
+	{
+		obj.transform.Translate(Vector3.up * Time.deltaTime);
+	}
+
+	if(GUILayout.Button("move down", GUILayout.Height(50)))
+	{
+		obj.transform.Translate(-Vector3.up * Time.deltaTime);
+	}
+
+	GUILayout.Label("the cube's position is: " + obj.transform.position);
+
+}
