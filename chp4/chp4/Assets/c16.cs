f33bea21c77dd244c40104d96311cc3f19e3c7f8
Freshield <zxdsw199182@gmail.com>
2016-07-26 17:45:55 -0400

    test 16

---
+++ chp4/chp4/Assets/c16.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class c16 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Debug.Log(Mathf.Abs(-3));
+        Debug.Log(Mathf.Sin(5));
+        Debug.Log(Mathf.Cos(5));
+        Debug.Log(Mathf.Tan(2));
+        Debug.Log(Mathf.Max(0, 100));
+        Debug.Log(Mathf.Min(0, 100));
+
+        Debug.Log(Mathf.PI);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
