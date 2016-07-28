6373c42721458ed904a8422dcc7a293d98e9517a
Freshield <zxdsw199182@gmail.com>
2016-07-28 17:52:48 -0400

    test3

--- chp5/chp5/chp5/Assets/c01.cs
+++ chp5/chp5/chp5/Assets/c01.cs
@@ -3,13 +3,27 @@ using System.Collections;
 
 public class c01 : MonoBehaviour {
 
+    private Camera camera;
+
 	// Use this for initialization
 	void Start () {
+
+        camera = gameObject.GetComponent<Camera>();
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void OnGUI () {
+
+        if (GUILayout.Button("first one",GUILayout.Height(50)))
+        {
+            camera.orthographic = true;
+        }
+
+        if (GUILayout.Button("second one", GUILayout.Height(50)))
+        {
+            camera.orthographic = false;
+        }
+
+    }
 }
