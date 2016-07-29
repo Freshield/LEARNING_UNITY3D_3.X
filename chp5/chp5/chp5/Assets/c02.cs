f768f0ae0eded93e86005e5699322cab61925097
Freshield <zxdsw199182@gmail.com>
2016-07-28 20:39:43 -0400

    test3

---
+++ chp5/chp5/chp5/Assets/c02.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class c02 : MonoBehaviour {
+
+    [MenuItem("new menu/clone choiced item")]
+    static void ClothObject()
+    {
+        Instantiate(Selection.activeTransform, Vector3.zero, Quaternion.identity);
+    }
+
+    [MenuItem("new menu/clone choiced item",true)]
+    static bool NoClothObject()
+    {
+        return Selection.activeGameObject != null;
+    }
+
+    [MenuItem("new menu/delete choiced item")]
+    static void RemoveObject()
+    {
+        DestroyImmediate(Selection.activeGameObject, true);
+    }
+
+    [MenuItem("new menu/delete choiced item",true)]
+    static bool NoRemoveObject()
+    {
+        return Selection.activeGameObject != null;
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
