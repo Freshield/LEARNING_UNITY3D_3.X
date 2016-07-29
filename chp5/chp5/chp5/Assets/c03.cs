2ba0c24bdfc1931f7d5ba5876c9c5835af72c8c2
Freshield <zxdsw199182@gmail.com>
2016-07-28 20:43:08 -0400

    test4

---
+++ chp5/chp5/chp5/Assets/c03.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[AddComponentMenu("new script/rotate auto")]
+public class c03 : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(0.0f, Time.deltaTime * 200, 0.0f);
+	
+	}
+}
