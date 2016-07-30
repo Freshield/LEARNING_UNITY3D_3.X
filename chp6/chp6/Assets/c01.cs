1ef9effbf381b8b32fe7be812725593342f82c38
Freshield <zxdsw199182@gmail.com>
2016-07-30 18:43:48 -0400

    test1

---
+++ chp6/chp6/Assets/c01.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class c01 : MonoBehaviour {
+
+    GameObject addFrceObj = null;
+
+    GameObject addPosObj = null;
+
+    GameObject cubeObj = null;
+
+	// Use this for initialization
+	void Start () {
+
+        addFrceObj = GameObject.Find("Sphere0");
+
+        addPosObj = GameObject.Find("Sphere1");
+
+        cubeObj = GameObject.Find("Cube");
+	
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+
+        if (GUILayout.Button("normal force",GUILayout.Height(50)))
+        {
+            addFrceObj.GetComponent<Rigidbody>().AddForce(1000, 0, 1000);
+        }
+
+        if (GUILayout.Button("poisition force",GUILayout.Height(50)))
+        {
+            Vector3 force = cubeObj.transform.position - addPosObj.transform.position;
+
+            force.y += 10;
+            force.x += 200;
+
+            addPosObj.GetComponent<Rigidbody>().AddForceAtPosition(force, addPosObj.transform.position, ForceMode.Impulse);
+        }
+	
+	}
+}
---
+++ chp6/chp6/Assets/c02.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class c02 : MonoBehaviour {
+
+    string show = null;
+
+	// Use this for initialization
+	void Start () {
+
+        show = "no collsion";
+	
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        show = "begin collisioning, name is " + collision.gameObject.name;
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        show = "collingsionING, name is " + collision.gameObject.name;
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        show = "after collision, name is " + collision.gameObject.name;
+        collision.gameObject.GetComponent<Rigidbody>().Sleep();
+    }
+	
+	// Update is called once per frame
+	void OnGUI () {
+
+        GUI.Label(new Rect(100, 0, 300, 40), show);
+	
+	}
+}
