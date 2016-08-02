01d2a1650c371d6ab22a96f0b4f1ba19a41105b7
Freshield <zxdsw199182@gmail.com>
2016-08-02 17:26:36 -0400

    interrupt

---
+++ map_test/test1/Assets/calculate.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class calculate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        float latitude = 45.495061f;
+        float longtitude = -73.578007f;
+        float ratio = Mathf.Cos(Mathf.Deg2Rad * latitude);
+        float onesecond = ((360 * 3600) / (512 * 2 * Mathf.Pow(2, 13)));
+        float halfimagedegree = (onesecond * 256 / 3600);
+        float fullimagedegree = halfimagedegree * 2;
+        float halfla = halfimagedegree * ratio;
+        float fullla = halfla * 2;
+
+        Debug.Log("2`13 = " + Mathf.Pow(2,13));
+        Debug.Log("one second per pixed = " + onesecond);
+        Debug.Log("half image = " + (onesecond * 256));
+        Debug.Log("half image degree = " + halfimagedegree);
+        Debug.Log("full image degree = " + fullimagedegree);
+        Debug.Log("ratio = " + ratio);
+        Debug.Log("half latitude degree = " + halfla);
+        Debug.Log("full latitude degree = " + fullla);
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- map_test/test1/Assets/test_get.cs
+++ map_test/test1/Assets/test_get.cs
@@ -1,12 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class test_get : MonoBehaviour {
+public class test_get : MonoBehaviour
+{
+    public enum MapType
+    {
+        RoadMap,
+        Satellite,
+        Terrain,
+        Hybrid
+    }
+    public bool loadOnStart = true;
+    public GMapLocation centerLocation;
+    public int zoom = 13;
+    public MapType mapType;
+    public int size = 512;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    void Start()
+    {
+        if (loadOnStart) Refresh();
+    }
 
     public void Refresh()
     {
@@ -16,26 +29,80 @@ public class test_get : MonoBehaviour {
 
     IEnumerator _Refresh()
     {
-        
-        string qs = "https://maps.googleapis.com/maps/api/staticmap?center=45.495061,-73.578007&zoom=13&size=512x512&scale=2&maptype=roadmap&markers=color:red%7Clabel:Y%7C45.495061,-73.578007&markers=color:blue%7Clabel:Y%7C45.495061,-73.622007&markers=color:green%7Clabel:Y%7C45.495061,-73.534007&markers=color:blue%7Clabel:Y%7C45.525905,-73.578007&markers=color:yellow%7Clabel:Y%7C45.464217,-73.578007&key=AIzaSyAWzOOJz0eZ8bs294s_PJdfOs8nz-s9xKc";
-        var req = new HTTP.Request("GET", qs, true);
+        float latitude = 45.495061f;
+        float longtitude = -73.578007f;
+        float ratio = Mathf.Cos(Mathf.Deg2Rad * latitude);
+        float onesecond = ((360 * 3600) / (512 * 2 * Mathf.Pow(2, 13)));
+        float halfimagedegree = (onesecond * 256 / 3600);
+        float fullimagedegree = halfimagedegree * 2;
+        float halfla = halfimagedegree * ratio;
+        float fullla = halfla * 2;
+
+        Debug.Log("centra latitude = " + centerLocation.latitude);
+        centerLocation.latitude += fullla;
+        Debug.Log("centra latitude = " + centerLocation.latitude);
+
+
+        string url = "https://maps.googleapis.com/maps/api/staticmap?";
+        string qs = "";
+        qs += "center=" + HTTP.URL.Encode(string.Format("{0},{1}", centerLocation.latitude, centerLocation.longitude));
+        qs += "&zoom=" + zoom.ToString();
+        qs += "&size=" + HTTP.URL.Encode(string.Format("{0}x{0}", size));
+        qs += "&scale=2";
+        qs += "&maptype=" + mapType.ToString().ToLower();
+        qs += "&key=AIzaSyAWzOOJz0eZ8bs294s_PJdfOs8nz-s9xKc";
+
+        //qs = "https://maps.googleapis.com/maps/api/staticmap?center=45.495061,-73.578007&zoom=13&size=512x512&scale=2&maptype=roadmap&markers=color:red%7Clabel:Y%7C45.495061,-73.578007&markers=color:blue%7Clabel:Y%7C45.495061,-73.622007&markers=color:green%7Clabel:Y%7C45.495061,-73.534007&markers=color:blue%7Clabel:Y%7C45.525905,-73.578007&markers=color:yellow%7Clabel:Y%7C45.464217,-73.578007&key=AIzaSyAWzOOJz0eZ8bs294s_PJdfOs8nz-s9xKc";
+        var req = new HTTP.Request("GET", url + qs, true);
+        Debug.Log(url + qs);
         req.Send();
         while (!req.isDone)
             yield return null;
         if (req.exception == null)
         {
-            var tex = new Texture2D(512, 512);
+            var tex = new Texture2D(size, size);
             tex.LoadImage(req.response.Bytes);
             GetComponent<Renderer>().material.mainTexture = tex;
         }
     }
 
 
-
-
-// Update is called once per frame
-void Update() {
-
 }
+
+public enum GMapColor
+{
+    black,
+    brown,
+    green,
+    purple,
+    yellow,
+    blue,
+    gray,
+    orange,
+    red,
+    white
+}
+
+[System.Serializable]
+public class GMapLocation
+{
+    public string address;
+    public float latitude;
+    public float longitude;
+}
+
+[System.Serializable]
+public class GMapMarker
+{
+    public enum GoogleMapMarkerSize
+    {
+        Tiny,
+        Small,
+        Mid
+    }
+    public GoogleMapMarkerSize size;
+    public GoogleMapColor color;
+    public string label;
+    public GoogleMapLocation[] locations;
 
 }
