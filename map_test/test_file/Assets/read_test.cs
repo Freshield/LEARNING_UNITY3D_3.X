5b7947bad31c8133c4fe5ef3505fad50dfec6e53
Freshield <zxdsw199182@gmail.com>
2016-08-01 22:06:42 -0400

    done today

---
+++ map_test/test_file/Assets/read_test.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+
+public class read_test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        ArrayList info = LoadFile(Application.dataPath, "TrajectoryStream.txt");
+
+        foreach (string str in info)
+        {
+            Debug.Log(str);
+        }
+
+        //for (int i = 0; i < 100; i++)
+        //{
+        //    Debug.Log(info.);
+        //}
+	
+	}
+
+    ArrayList LoadFile(string path, string name)
+    {
+        StreamReader sr = null;
+        try
+        {
+            sr = File.OpenText(path + "//" + name);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+            return null;
+        }
+
+        string line;
+        ArrayList arrlist = new ArrayList();
+        int number = 0;
+        while ((line = sr.ReadLine()) != null)
+        {
+            arrlist.Add(line);
+            number++;
+            if (number > 120)
+            {
+                break;
+            }
+        }
+        sr.Close();
+        sr.Dispose();
+        return arrlist;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ map_test/test_file/Assets/write_test.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class write_test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        CreateFile(Application.dataPath, "FileName.txt", "TestInfo0233");
+        CreateFile(Application.dataPath, "FileName.txt", "TestInfo1233");
+        CreateFile(Application.dataPath, "FileName.txt", "TestInfo2233");
+
+    }
+
+    void CreateFile(string path, string name, string info)
+    {
+        StreamWriter sw;
+        FileInfo t = new FileInfo(path + "//" + name);
+        if (!t.Exists)
+        {
+            sw = t.CreateText();
+        }
+        else
+        {
+            sw = t.AppendText();
+        }
+
+        sw.WriteLine(info);
+        sw.Close();
+        sw.Dispose();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
