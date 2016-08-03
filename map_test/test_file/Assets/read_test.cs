91e075b7e9de666d3e87af8e2cb5325afd3af740
Freshield <zxdsw199182@gmail.com>
2016-08-03 17:30:10 -0400

    test read file

--- map_test/test_file/Assets/read_test.cs
+++ map_test/test_file/Assets/read_test.cs
@@ -15,12 +15,29 @@ public class read_test : MonoBehaviour {
             Debug.Log(str);
         }
 
+        string test = "Hello!!!!!:";
+        Debug.Log(test);
+        int pos;
+
+        if ((pos = test.IndexOf(":")) != -1)
+        {
+            Debug.Log(test.Remove(pos));
+        }
+
+        test = "39.984428,116.307126,360";
+        Debug.Log(test);
+        string[] result = test.Split(',');
+        foreach (string item in result)
+        {
+            Debug.Log(item);
+        }
+
         //for (int i = 0; i < 100; i++)
         //{
         //    Debug.Log(info.);
         //}
-	
-	}
+
+    }
 
     ArrayList LoadFile(string path, string name)
     {
@@ -42,7 +59,7 @@ public class read_test : MonoBehaviour {
         {
             arrlist.Add(line);
             number++;
-            if (number > 120)
+            if (number > 20)
             {
                 break;
             }
---
+++ map_test/test_file/Assets/track_test.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+
+public class track_test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    ArrayList LoadFile(string path, string name)
+    {
+        StreamReader sr = null;
+        try
+        {
+            sr = File.OpenText(path + "//" + name);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+            return null;
+        }
+
+        string line;
+        Track temp;
+        int pos;
+        
+        ArrayList arrlist = new ArrayList();
+        while ((line = sr.ReadLine()) != null)
+        {
+            if ((pos = line.IndexOf(":")) != -1)
+            {
+                Track track = new Track(line.Remove(pos));
+                temp = track;
+            }
+            else if (true)
+            {
+
+            }
+        }
+        sr.Close();
+        sr.Dispose();
+        return arrlist;
+
+    }
+}
+
+public class Track
+{
+    public string name;
+    public Position[] positions;
+
+    public Track(string name)
+    {
+        this.name = name;
+    }
+
+
+}
+
+public class Position
+{
+    public float latitute;
+    public float lontitute;
+    public int time;
+
+    public Position(float latitute,float lontitute,int time)
+    {
+        this.latitute = latitute;
+        this.lontitute = lontitute;
+        this.time = time;
+    }
+}
