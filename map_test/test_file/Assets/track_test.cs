93b237ed133af3b4ebe6b7637ccad6fadaa4e9a3
Freshield <zxdsw199182@gmail.com>
2016-08-03 17:46:09 -0400

    done create object

--- map_test/test_file/Assets/track_test.cs
+++ map_test/test_file/Assets/track_test.cs
@@ -7,8 +7,20 @@ public class track_test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+
+        ArrayList tracks = LoadFile(Application.dataPath, "TrajectoryStream.txt");
+
+        foreach (Track track in tracks)
+        {
+            Debug.Log(track.name);
+            if (track.positions.Count > 0)
+            {
+                Position position = (Position)track.positions[0];
+                Debug.Log(position.latitute + "," + position.lontitute + "," + position.time);
+            }
+        }
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -28,25 +40,28 @@ public class track_test : MonoBehaviour {
         }
 
         string line;
-        Track temp;
+        Track temp = new Track("temp");
         int pos;
         
-        ArrayList arrlist = new ArrayList();
+        ArrayList tracks = new ArrayList();
         while ((line = sr.ReadLine()) != null)
         {
             if ((pos = line.IndexOf(":")) != -1)
             {
                 Track track = new Track(line.Remove(pos));
                 temp = track;
+                tracks.Add(track);
             }
-            else if (true)
+            else if (line.Contains(","))
             {
-
+                string[] result = line.Split(',');
+                Position position = new Position(float.Parse(result[0]), float.Parse(result[1]), int.Parse(result[2]));
+                temp.positions.Add(position);
             }
         }
         sr.Close();
         sr.Dispose();
-        return arrlist;
+        return tracks;
 
     }
 }
@@ -54,11 +69,12 @@ public class track_test : MonoBehaviour {
 public class Track
 {
     public string name;
-    public Position[] positions;
+    public ArrayList positions;
 
     public Track(string name)
     {
         this.name = name;
+        positions = new ArrayList();
     }
 
 
