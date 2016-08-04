92fe4c975ee814d2f46f380b5bd8cba260974367
Freshield <zxdsw199182@gmail.com>
2016-08-04 18:45:58 -0400

    add mouse

--- map_test/test1/Assets/locateBall.cs
+++ map_test/test1/Assets/locateBall.cs
@@ -3,29 +3,59 @@ using System.Collections;
 
 public class locateBall : MonoBehaviour {
 
-    public GameObject prefab;
+    
+    public Position center;
+    public float fullLat;
+    public float fullLon;
+    public float radius = GameObject.Find("Prefab").transform.localScale.x / 2;
+    string name;
+
+    public locateBall(Position center, float fullLat, float fullLon,string name)
+    {
+        this.center = center;
+        this.fullLat = fullLat;
+        this.fullLon = fullLon;
+        this.name = name;
+    }
+
+    public GameObject putBall(ArrayList balls, ArrayList locations)
+    {
+        GameObject parent = new GameObject(name);
+
+        for (int i = 0; i < balls.Count; i++)
+        {
+            locate((GameObject)balls[i], parent, (Position)locations[i]);
+        }
+
+        return parent;
+
+    }
+
+    public ArrayList addBall(GameObject prefab,int length)
+    {
+        ArrayList balls = new ArrayList();
+        for (int i = 0; i < length; i++)
+        {
+            GameObject ball = Instantiate(prefab);
+            balls.Add(ball);
+        }
+        return balls;
+    }
+
+    public void locate(GameObject ball, GameObject parent, Position position)
+    {
+        
+        float x = (position.lontitute - center.lontitute) * 10 / fullLon;
+        float y = (position.latitute - center.latitute) * 10 / fullLat;
+        ball.transform.position = new Vector3(x, y, -radius);
+        ball.transform.parent = parent.transform;
+        //Debug.Log(ball.name + " " + ball.transform.position + " " + ball.transform.parent);
+
+    }
 
 	// Use this for initialization
 	void Start () {
-
-        GameObject ball = new GameObject();
         
-
-        GameObject o0 = Instantiate(prefab);
-        GameObject o1 = Instantiate(prefab);
-        GameObject o2 = Instantiate(prefab);
-        GameObject o3 = Instantiate(prefab);
-        
-        o0.transform.position = new Vector3(-10, 10, -0.5f);
-        o1.transform.position = new Vector3(0, 10, -0.5f);
-        o2.transform.position = new Vector3(10, 10, -0.5f);
-        o3.transform.position = new Vector3(-10, 0, -0.5f);
-
-        o0.transform.parent = ball.transform;
-        o1.transform.parent = ball.transform;
-        o2.transform.parent = ball.transform;
-        o3.transform.parent = ball.transform;
-
 
     }
 	
