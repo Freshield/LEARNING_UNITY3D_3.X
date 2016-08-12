f3716b1891393dcbc1089bf0d5f942460f7189f4
Freshield <zxdsw199182@gmail.com>
2016-08-12 13:34:14 -0400

    test ray point

--- chp6/chp6/Assets/c04.cs
+++ chp6/chp6/Assets/c04.cs
@@ -11,10 +11,21 @@ public class c04 : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        Ray ray = new Ray(Vector3.zero, transform.position);
-        RaycastHit hit;
-        Physics.Raycast(ray, out hit, 100);
-        Debug.DrawLine(ray.origin, hit.point);
+        if(Input.GetMouseButton(0))
+         {
+             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);//从摄像机发出到点击坐标的射线
+             RaycastHit hitInfo;
+             if(Physics.Raycast(ray,out hitInfo))
+             {
+                 Debug.DrawLine(ray.origin,hitInfo.point);//划出射线，只有在scene视图中才能看到
+                 GameObject gameObj = hitInfo.collider.gameObject;
+                 Debug.Log("click object name is " + gameObj.name);
+                 if(gameObj.tag == "boot")//当射线碰撞目标为boot类型的物品 ，执行拾取操作
+                 {
+                     Debug.Log("pick up!");
+                 }
+             }
+         }
 	
 	}
 }
