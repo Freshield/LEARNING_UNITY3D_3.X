9ded6fa28e380ae47727cb5325b603374b43c047
Freshield <zxdsw199182@gmail.com>
2016-08-11 21:50:17 -0400

    test character

---
+++ chp6/chp6/Assets/c03.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class c03 : MonoBehaviour {
+
+    CharacterController controller = null;
+
+    float moveSpeed = 30.0f;
+
+    float rotateSpeed = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+
+        controller = GetComponent<CharacterController>();
+	
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+
+        if (GUILayout.RepeatButton("left rotate"))
+        {
+            transform.Rotate(0, -rotateSpeed, 0);
+        }
+
+        if (GUILayout.RepeatButton("right rotate"))
+        {
+            transform.Rotate(0, rotateSpeed, 0);
+        }
+
+        if (GUILayout.RepeatButton("forward"))
+        {
+            controller.SimpleMove(Vector3.forward * moveSpeed);
+        }
+
+        if (GUILayout.RepeatButton("backward"))
+        {
+            controller.SimpleMove(Vector3.back * moveSpeed);
+        }
+
+        if (GUILayout.RepeatButton("left"))
+        {
+            controller.SimpleMove(Vector3.left * moveSpeed);
+        }
+
+        if (GUILayout.RepeatButton("right"))
+        {
+            controller.SimpleMove(Vector3.right * moveSpeed);
+        }
+
+    }
+}
