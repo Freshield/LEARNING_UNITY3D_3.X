95b145383156442730b64ed2b0c721039baf65bd
Freshield <zxdsw199182@gmail.com>
2016-08-11 22:41:22 -0400

    done today

---
+++ chp6/chp6/Assets/c05.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class c05 : MonoBehaviour {
+
+    public Texture texture;
+
+    public string info;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            info = "hit";
+        }
+        else
+        {
+            info = "not hit";
+        }
+	
+	}
+
+    void OnGUI()
+    {
+        Rect rect = new Rect(Input.mousePosition.x - (texture.width >> 1), Screen.height - Input.mousePosition.y - (texture.height >> 1), texture.width, texture.height);
+
+        GUI.DrawTexture(rect, texture);
+
+        GUILayout.Label(info + ",position is: " + Input.mousePosition);
+    }
+}
