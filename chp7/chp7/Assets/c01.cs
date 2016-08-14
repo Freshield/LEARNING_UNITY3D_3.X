506558ae14826fb5ad5cb4aec1ebd100684826cb
Freshield <zxdsw199182@gmail.com>
2016-08-14 17:29:29 -0400

    test hold key

--- chp7/chp7/Assets/c01.cs
+++ chp7/chp7/Assets/c01.cs
@@ -10,6 +10,59 @@ public class c01 : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("press w");
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("press s");
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("press a");
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("press d");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("press space");
+        }
+
+        ///////////////////////////////////////////////////
+
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            Debug.Log("up w");
+        }
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            Debug.Log("up s");
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            Debug.Log("up a");
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            Debug.Log("up d");
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Debug.Log("up space");
+        }
+
+    }
 }
---
+++ chp7/chp7/Assets/c02.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class c02 : MonoBehaviour {
+
+    int keyFrame = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("press a");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            keyFrame++;
+            Debug.Log("a press continue " + keyFrame + " frames");
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            keyFrame = 0;
+            Debug.Log("a up");
+        }
+	
+	}
+}
