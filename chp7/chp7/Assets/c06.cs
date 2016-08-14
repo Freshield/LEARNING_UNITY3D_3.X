011fdf1094ca7769dafecf1f1581c525fa987e1f
Freshield <zxdsw199182@gmail.com>
2016-08-14 19:49:15 -0400

    mouse up

---
+++ chp7/chp7/Assets/c06.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class c06 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("press left on " + Input.mousePosition);
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            Debug.Log("press right on " + Input.mousePosition);
+        }
+
+        if (Input.GetMouseButtonUp(2))
+        {
+            Debug.Log("press middle on " + Input.mousePosition);
+        }
+
+
+    }
+}
