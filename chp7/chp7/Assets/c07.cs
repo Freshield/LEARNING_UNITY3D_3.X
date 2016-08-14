82e1ae636ff513ba519ba0603991cf72ac9072cf
Freshield <zxdsw199182@gmail.com>
2016-08-14 19:52:49 -0400

    long press

---
+++ chp7/chp7/Assets/c07.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class c07 : MonoBehaviour {
+
+    int MouseFrame = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButton(0))
+        {
+            MouseFrame++;
+            Debug.Log("press left on " + MouseFrame + " frame");
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            MouseFrame++;
+            Debug.Log("press right on " + MouseFrame + " frame");
+        }
+
+        if (Input.GetMouseButton(2))
+        {
+            MouseFrame++;
+            Debug.Log("press middle on " + MouseFrame + " frame");
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            MouseFrame = 0;
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            MouseFrame = 0;
+        }
+
+        if (Input.GetMouseButtonUp(2))
+        {
+            MouseFrame = 0;
+        }
+
+    }
+}
