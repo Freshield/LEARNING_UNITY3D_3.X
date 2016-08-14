a0fbd8cb8e9faf2cfc5409d58c7f3d2b64d2b569
Freshield <zxdsw199182@gmail.com>
2016-08-14 17:13:06 -0400

    done chp6

--- chp6/chp6_2/chp6_2/Assets/c09.cs
+++ chp6/chp6_2/chp6_2/Assets/c09.cs
@@ -3,13 +3,43 @@ using System.Collections;
 
 public class c09 : MonoBehaviour {
 
+    private GameObject obj;
+
+    public Texture texture;
+
 	// Use this for initialization
 	void Start () {
+
+        obj = GameObject.Find("Sphere");
+
+        Cursor.visible = false;
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.name == "cube")
+                {
+                    Vector3 direction = hit.transform.position - obj.transform.position;
+
+                    obj.GetComponent<Rigidbody>().AddForceAtPosition(direction, hit.transform.position, ForceMode.Impulse);
+                }
+            }
+        }
 	
 	}
+
+    void OnGUI()
+    {
+        Rect rect = new Rect(Input.mousePosition.x - (texture.width >> 1), Screen.height - Input.mousePosition.y - (texture.height >> 1), texture.width, texture.height);
+
+        GUI.DrawTexture(rect, texture);
+    }
 }
