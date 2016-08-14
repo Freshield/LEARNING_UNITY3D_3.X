21561da51bb4ab995ef5c9278013b14d025d0ef4
Freshield <zxdsw199182@gmail.com>
2016-08-14 19:47:41 -0400

    test mouse down

---
+++ chp7/chp7/Assets/c05.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class c05 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("press left on " + Input.mousePosition);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("press right on " + Input.mousePosition);
+        }
+
+        if (Input.GetMouseButtonDown(2))
+        {
+            Debug.Log("press middle on " + Input.mousePosition);
+        }
+
+    }
+}
