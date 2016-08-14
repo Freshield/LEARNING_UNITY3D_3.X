64e3d740c2cebc628f6153657215cfdc9e3a34ba
Freshield <zxdsw199182@gmail.com>
2016-08-14 17:32:47 -0400

    test any key

---
+++ chp7/chp7/Assets/c03.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class c03 : MonoBehaviour {
+
+    int keyFrame = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.anyKeyDown)
+        {
+            keyFrame = 0;
+            Debug.Log("pressed");
+        }
+        if (Input.anyKey)
+        {
+            keyFrame++;
+            Debug.Log("press continue " + keyFrame + " frames");
+        }
+
+    }
+}
