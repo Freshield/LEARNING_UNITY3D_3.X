a7f84d17907809cb354b15f912583ab31f958787
Freshield <zxdsw199182@gmail.com>
2016-08-14 17:00:30 -0400

    test lot

---
+++ chp6/chp6_2/chp6_2/Assets/c08.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class c08 : MonoBehaviour {
+
+    private TrailRenderer trialRender;
+
+	// Use this for initialization
+	void Start () {
+
+        trialRender = gameObject.GetComponent<TrailRenderer>();
+	
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+
+        if (GUILayout.Button("add width",GUILayout.Height(50)))
+        {
+            trialRender.startWidth += 1;
+            trialRender.endWidth += 1;
+        }
+
+        if (GUILayout.Button("show path", GUILayout.Height(50)))
+        {
+            trialRender.enabled = true;
+        }
+
+
+        if (GUILayout.Button("hide path", GUILayout.Height(50)))
+        {
+            trialRender.enabled = false;
+        }
+
+    }
+}
