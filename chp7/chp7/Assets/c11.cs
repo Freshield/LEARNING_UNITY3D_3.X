342f614229849ce09406bb92ce7984bb2bad6e6c
Freshield <zxdsw199182@gmail.com>
2016-08-14 22:38:05 -0400

    interrupt

---
+++ chp7/chp7/Assets/c11.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class c11 : MonoBehaviour {
+
+    public Material material;
+
+    void OnPostRender()
+    {
+        if (!material)
+        {
+            Debug.Log("need material");
+            return;
+        }
+
+        material.SetPass(0);
+
+        GL.LoadOrtho();
+
+        GL.Begin(GL.LINES);
+
+        
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
