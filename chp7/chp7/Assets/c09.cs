f944d13fb88a62a8e165191e66137416cad69282
Freshield <zxdsw199182@gmail.com>
2016-08-14 20:47:10 -0400

    test rotation

---
+++ chp7/chp7/Assets/c09.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class c09 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float value = Input.GetAxis("test");
+        Debug.Log("axis is " + value);
+	
+	}
+}
---
+++ chp7/chp7/Assets/c10.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class c10 : MonoBehaviour {
+
+    public Transform target;
+
+    public float distance = 20.0f;
+
+    float x;
+    float y;
+
+    float yMinLimit = -20.0f;
+    float yMaxLimit = 80.0f;
+
+    float xSpeed = 250.0f;
+    float ySpeed = 120.0f;
+
+	// Use this for initialization
+	void Start () {
+
+        Vector2 angles = transform.eulerAngles;
+        x = angles.y;
+        y = angles.x;
+        Debug.Log("fist x y " + x + "," + y);
+
+        if (GetComponent<Rigidbody>())
+        {
+            GetComponent<Rigidbody>().freezeRotation = true;
+        }
+	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        if (Input.anyKey)
+        {
+            if (target)
+            {
+                x += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+                Debug.Log("mouse x " + Input.GetAxis("Mouse X"));
+                y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+                Debug.Log("mouse y " + Input.GetAxis("Mouse Y"));
+                y = ClampAngle(y, yMinLimit, yMaxLimit);
+                Quaternion rotation = Quaternion.Euler(y, x, 0);
+                Vector3 position = rotation * new Vector3(0.0f, 0.0f, -distance) + target.position;
+
+                transform.rotation = rotation;
+                transform.position = position;
+            }
+        }
+        
+	
+	}
+
+    float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360)
+        {
+            angle += 360;
+        }
+        if (angle > 360)
+        {
+            angle -= 360;
+        }
+        return Mathf.Clamp(angle, min, max);
+    }
+}
