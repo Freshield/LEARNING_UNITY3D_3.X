00ff9ea79db49e78c788acdf382b76ef7185f5e1
Freshield <zxdsw199182@gmail.com>
2016-08-14 20:15:07 -0400

    test new button

---
+++ chp7/chp7/Assets/c08.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class c08 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("test"))
+        {
+            Debug.Log("press left on " + Input.mousePosition);
+        }
+
+        if (Input.GetButton("test"))
+        {
+            Debug.Log("press right on " + Input.mousePosition);
+        }
+
+        if (Input.GetButton("test"))
+        {
+            Debug.Log("press middle on " + Input.mousePosition);
+        }
+    }
+}
