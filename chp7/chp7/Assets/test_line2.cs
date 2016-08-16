196c0bf4309835db8b79f461899dd02a7c90cd50
Freshield <zxdsw199182@gmail.com>
2016-08-15 23:36:41 -0400

    test line

---
+++ chp7/chp7/Assets/test_line2.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class test_line2 : MonoBehaviour {
+    // Use this for initialization  
+
+    private List<Vector3> list;
+    private bool IsDraw = false;
+    private LineRenderer lineRenderer;
+
+
+
+    void Start()
+    {
+        lineRenderer = gameObject.GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame  
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (list == null)
+                list = new List<Vector3>();
+
+            list.Clear();
+            IsDraw = true;
+            lineRenderer.SetVertexCount(0);
+
+        }
+        if (Input.GetMouseButton(1))
+        {
+            list.Add(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)));
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            IsDraw = false;
+        }
+
+
+
+        drawBezierCurve();
+        //drawInputPointCurve();  
+
+    }
+
+    private void drawBezierCurve()
+    {
+        if (IsDraw && list.Count > 0)
+        {
+            List<Vector3> bcList;
+            //          BezierCurve bc= new BezierCurve();  
+            
+            BezierPath bc = new BezierPath();
+
+            //bcList = bc.CreateCurve(list);//  通过贝塞尔曲线 平滑  
+            bcList = bc.CreateCurve(list);//  通过贝塞尔曲线 平滑  
+
+            lineRenderer.SetVertexCount(bcList.Count);
+            for (int i = 0; i < bcList.Count; i++)
+            {
+                Vector3 v = bcList[i];
+                v += new Vector3(0, 0.5f, 0);
+                lineRenderer.SetPosition(i, v);
+            }
+
+        }
+
+    }
+}
---
+++ chp7/chp7/Assets/test_linerender.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test_linerender : MonoBehaviour {
+
+    GameObject lineRenderObj;
+
+    LineRenderer lineRender;
+
+    int lineCount = 4;
+
+    Vector3 v0 = new Vector3(1, 0, 0);
+    Vector3 v1 = new Vector3(0, 1, 0);
+    Vector3 v2 = new Vector3(0, 0, 1);
+    Vector3 v3 = new Vector3(1, 0, 0);
+
+    ArrayList positions;
+
+    // Use this for initialization
+    void Start () {
+
+        lineRenderObj = GameObject.Find("line");
+
+        lineRender = lineRenderObj.GetComponent<LineRenderer>();
+
+        lineRender.SetVertexCount(lineCount);
+
+        lineRender.SetWidth(0.2f, 0.2f);
+
+        positions = new ArrayList{ v0, v1, v2, v3 };
+        
+
+        lineRender.SetPositions((Vector3[])positions.ToArray(typeof(Vector3)));
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("add",GUILayout.Height(50)))
+        {
+            lineCount++;
+            lineRender.SetVertexCount(lineCount);
+            Vector3 temp = new Vector3(Random.Range(-3, 3), Random.Range(-3, 3), Random.Range(-3, 3));
+            positions.Add(temp);
+            lineRender.SetPositions((Vector3[])positions.ToArray(typeof(Vector3)));
+        }
+    }
+}
