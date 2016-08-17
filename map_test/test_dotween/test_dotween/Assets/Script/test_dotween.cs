252d6357966c4a9597cf49a0be397766b8543cff
Freshield <zxdsw199182@gmail.com>
2016-08-17 18:32:29 -0400

    test track

--- map_test/test_dotween/test_dotween/Assets/Script/test_dotween.cs
+++ map_test/test_dotween/test_dotween/Assets/Script/test_dotween.cs
@@ -8,21 +8,36 @@ public class test_dotween : MonoBehaviour {
 
     GameObject obj;
 
+    GameObject train;
+
     Tweener tweener;
+
+    Vector3 startPosition;
 
     public float hSliderValue = 0;
 
     // Use this for initialization
     void Start () {
 
-        positions = new Vector3[5];
+        positions = new Vector3[10];
 
-        for (int i = 0; i < positions.Length; i++)
+        for (int i = 0; i < 5; i++)
         {
-            positions[i] = new Vector3(i, i, 0);
+            positions[i] = new Vector3(0, i, 0);
         }
+        positions[5] = new Vector3(0, 4, 1);
+        positions[6] = new Vector3(0, 4, 5);
+        positions[7] = new Vector3(0, 5, 3);
+        positions[8] = new Vector3(0, 2, 4);
+        positions[9] = new Vector3(0, 1, 6);
+
+
 
         obj = GameObject.Find("Cube");
+
+        startPosition = obj.transform.position;
+
+        train = GameObject.Find("CubeTrain");
 
         tweener = obj.transform.DOPath(positions, 4, PathType.CatmullRom, PathMode.Full3D, 5, null);
 
@@ -36,6 +51,8 @@ public class test_dotween : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        
 	
 	}
 
@@ -60,17 +77,44 @@ public class test_dotween : MonoBehaviour {
         {
             GUILayout.HorizontalSlider(tweener.fullPosition, 0, 4, GUILayout.Width(200));
             hSliderValue = tweener.fullPosition;
+            drawLine();
         }
         else
         {
             hSliderValue = GUILayout.HorizontalSlider(hSliderValue, 0, 4, GUILayout.Width(200));
 
             tweener.Goto(hSliderValue, false);
+
+            drawLine();
         }
 
-        Debug.Log(tweener.fullPosition);
-        
-        
+        //Debug.Log(tweener.fullPosition);
+        Debug.Log(tweener.PathLength());
+
+
+
+    }
+
+    void drawLine()
+    {
+        float timeNow = tweener.fullPosition;
+        float percent = timeNow / 4;
+        Vector3 nowPosition = tweener.PathGetPoint(percent);
+
+        ArrayList positions = new ArrayList();
+        //positions.Add(nowPosition);
+
+        float count = timeNow;
+
+        while (count > 0)
+        {
+            float per = count / 4;
+            positions.Add(tweener.PathGetPoint(per));
+            count -= 0.02f;
+        }
+
+        obj.GetComponent<LineRenderer>().SetVertexCount(positions.Count);
+        obj.GetComponent<LineRenderer>().SetPositions((Vector3[])positions.ToArray(typeof(Vector3)));
 
     }
 }
---
+++ map_test/test_dotween/test_dotween/Assets/test_linerender.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test_linerender : MonoBehaviour {
+    public Color c1 = Color.yellow;
+    public Color c2 = Color.red;
+    private int LengthOflineRenderer = 0;
+    private int index = 0;
+    LineRenderer lineRend;
+    Vector3 position;
+    // Use this for initialization
+    void Start()
+    {
+        lineRend = gameObject.AddComponent<LineRenderer>();
+        lineRend.SetColors(c1, c2);
+        lineRend.SetWidth(0.1f, 0.1f);
+        lineRend.SetVertexCount(LengthOflineRenderer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lineRend = GetComponent<LineRenderer>();
+        if (Input.GetMouseButtonDown(0))
+        {
+            print("1");
+            position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1.0f);
+            LengthOflineRenderer++;
+            lineRend.SetVertexCount(LengthOflineRenderer);
+        }
+        //while (index
+         print("2");
+        lineRend.SetPosition(index, position);
+        index++;
+    }
+
+void OnGUI()
+{
+    GUILayout.Label("鼠标的x轴" + Input.mousePosition.x);
+    GUILayout.Label("鼠标的y轴" + Input.mousePosition.y);
+}
+}
