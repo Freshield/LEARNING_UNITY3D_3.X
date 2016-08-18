3b5803cd352a2dcd9fdb74a9300aa317ee0ab840
Freshield <zxdsw199182@gmail.com>
2016-08-18 14:35:04 -0400

    creat PTime, Position

---
+++ map_test/test2/test2/Assets/Script/PlaneCreator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneCreator : MonoBehaviour {
+    public GameObject[] planes;
+    public GameObject plane_parent;
+    public GameObject prefab;
+
+    public PlaneCreator(Vector3 center, int x, int y, float width)
+    {
+        planes = new GameObject[x * y];
+
+        plane_parent = new GameObject("plane_parent");
+
+        for (int i = 0; i < y; i++)
+        {
+            for (int j = 0; j < x; j++)
+            {
+                GameObject plane = Instantiate(prefab);
+                plane.name = "plane" + (x * i + j);
+                plane.transform.parent = plane_parent.transform;
+                float hor = (((-x + 1) * width) / 2) + (width * j);
+                float ver = (((y - 1) * width) / 2) - (width * i);
+                //Debug.Log(plane.name + " " + hor + "," + ver);
+                plane.transform.position = new Vector3(hor + center.x, ver + center.y, center.z);
+                planes[x * i + j] = plane;
+            }
+        }
+
+    }
+}
