7cefc212b90681bfccb46906c537db5114fe9f95
Freshield <zxdsw199182@gmail.com>
2016-08-25 22:01:51 -0400

    interrupt

--- map_test/test_dotween/test_dotween/Assets/Script/test_sequence.cs
+++ map_test/test_dotween/test_dotween/Assets/Script/test_sequence.cs
@@ -8,14 +8,23 @@ public class test_sequence : MonoBehaviour {
 
     Tweener tweener;
 
-    Sequence sequence;
+    Tweener sequence;
+
+    Tweener Vsequence;
 
     Vector3[] positions;
+
+    Vector3 myVector;
+    Vector3 mVector;
 
     public float hSliderValue = 0;
 
     // Use this for initialization
     void Start () {
+
+
+        DOTween.defaultAutoPlay = AutoPlay.None;
+
 
         positions = new Vector3[10];
 
@@ -31,48 +40,94 @@ public class test_sequence : MonoBehaviour {
 
         obj = GameObject.Find("Cube");
 
-        sequence = DOTween.Sequence();
+        myVector = obj.transform.position;
+        mVector = obj.transform.position;
 
-        DOTween.defaultAutoPlay = AutoPlay.None;
 
-        for (int i = 0; i < positions.Length; i++)
-        {
-            sequence.Append(obj.transform.DOMove(positions[i], 1, false));
-        }
+        Vector3[] endValues = positions;
+        float[] durations = new[] { 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f};
+        Vsequence = DOTween.ToArray(() => myVector, x => myVector = x, endValues, durations);
+        sequence = DOTween.ToArray(() => mVector, x => mVector = x, endValues, durations);
 
+        Vsequence.SetAutoKill(false).SetEase(Ease.Linear);
         sequence.SetAutoKill(false).SetEase(Ease.Linear);
-        
-	
-	}
-	
-	// Update is called once per frame
-	void OnGUI () {
 
+        Vsequence.Pause();
+        sequence.Pause();
+
+
+
+    }
+
+    // Update is called once per frame
+    void OnGUI () {
+        
         GUILayout.Label(obj.transform.position.ToString());
+        GUILayout.Label(myVector.ToString());
 
 
         if (GUILayout.Button("play", GUILayout.Height(50)))
         {
+            Vsequence.PlayForward();
             sequence.PlayForward();
+
         }
 
         if (GUILayout.Button("pause", GUILayout.Height(50)))
         {
+            Vsequence.Pause();
             sequence.Pause();
         }
 
 
-        if (sequence.IsPlaying())
+        if (Vsequence.IsPlaying())
         {
-            GUILayout.HorizontalSlider(sequence.fullPosition, 0, 10, GUILayout.Width(200));
-            hSliderValue = sequence.fullPosition;
+            GUILayout.HorizontalSlider(Vsequence.fullPosition, 0, 10, GUILayout.Width(200));
+            hSliderValue = Vsequence.fullPosition;
         }
         else
         {
             hSliderValue = GUILayout.HorizontalSlider(hSliderValue, 0, 10, GUILayout.Width(200));
 
+            Vsequence.Goto(hSliderValue, false);
             sequence.Goto(hSliderValue, false);
         }
+
+    }
+
+    void Update()
+    {
+
+        obj.transform.position = myVector;
+
+        drawLine();
+
+    }
+
+    void drawLine()
+    {
+
+        float timeNow = Vsequence.fullPosition;
+        
+
+        ArrayList positions = new ArrayList();
+        //positions.Add(nowPosition);
+
+        
+        float count = timeNow;
+        
+        while (count > 0)
+        {
+            sequence.Goto(count);
+            positions.Add(mVector);
+            count -= 0.02f;
+        }
+
+        sequence.Goto(timeNow);
+
+
+        obj.GetComponent<LineRenderer>().SetVertexCount(positions.Count);
+        obj.GetComponent<LineRenderer>().SetPositions((Vector3[])positions.ToArray(typeof(Vector3)));
 
     }
 }
