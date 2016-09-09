02ee77cdf9291ba631f9d92fd96f914f01bcbab1
Freshield <zxdsw199182@gmail.com>
2016-09-09 16:04:16 -0400

    test highlight

---
+++ map_test/test3/test3_prepared/Assets/test_hit.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test_hit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray;
+        RaycastHit hit;
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject != null)
+            {
+                HighlightableObject ho = hit.collider.transform.GetComponentInChildren<HighlightableObject>();
+                if (ho != null)
+                {
+                    ho.On(Color.red);
+                }
+            }
+        }
+
+    }
+}
