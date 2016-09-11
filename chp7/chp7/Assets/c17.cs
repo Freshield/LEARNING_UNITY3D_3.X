864f9f9e69f2bbf0de7368a2667aa21d445c33c6
Freshield <zxdsw199182@gmail.com>
2016-09-10 20:54:39 -0400

    test quad

---
+++ chp7/chp7/Assets/c17.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class c17 : MonoBehaviour {
+
+    public Material mat0;
+    public Material mat1;
+    public Material mat2;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
