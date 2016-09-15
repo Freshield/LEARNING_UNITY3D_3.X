4fa8ca9be5b29ed4d380ecd34ba70f79a8d112c4
Freshield <zxdsw199182@gmail.com>
2016-09-14 20:23:58 -0400

    test gl line

--- chp7/chp7/Assets/c15.cs
+++ chp7/chp7/Assets/c15.cs
@@ -3,6 +3,37 @@ using System.Collections;
 
 public class c15 : MonoBehaviour {
 
+    public Material material;
+
+    void OnPostRender()
+    {
+        if (!material)
+        {
+            Debug.Log("null material");
+            return;
+        }
+
+        material.SetPass(0);
+
+        GL.LoadOrtho();
+
+        GL.Begin(GL.LINES);
+
+        DrawLine(0, 0, 200, 100);
+
+        DrawLine(0, 50, 200, 150);
+
+        DrawLine(0, 100, 200, 200);
+
+        GL.End();
+    }
+
+    void DrawLine(float x1, float y1, float x2, float y2)
+    {
+        GL.Vertex(new Vector3(x1 / Screen.width, y1 / Screen.height, 0));
+        GL.Vertex(new Vector3(x2 / Screen.width, y2 / Screen.height, 0));
+    }
+
 	// Use this for initialization
 	void Start () {
 	
