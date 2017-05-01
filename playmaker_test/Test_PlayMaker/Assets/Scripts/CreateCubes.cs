87ed96d6a2b9a8bcdcdb2939b10013a926c589db
Freshield <zxdsw199182@gmail.com>
2017-05-01 19:26:39 -0400

    done import

---
+++ playmaker_test/Test_PlayMaker/Assets/Scripts/CreateCubes.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateCubes : MonoBehaviour {
+
+    public GameObject cubePrefab;
+    public GameObject cubeParent;
+
+	// Use this for initialization
+	void Start () {
+        for(int i = 0; i < 10; i++)
+        {
+            GameObject cube = Instantiate(cubePrefab);
+            cube.name = i.ToString();
+            cube.transform.position = new Vector3(i + (0.2f * i), 0, 0);
+            cube.transform.parent = cubeParent.transform;
+
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
